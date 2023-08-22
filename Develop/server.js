4098fb1725a8b81b58fe79250b17d9e0ec36ed81
Mark Anthony <touya1313@hotmail.com>
2023-08-22 15:33:13 -0600

    added middleware

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,9 @@ app.listen(3001)
 
 const app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(PORT, () =>
-  console.log(`Listening for requests on port ${PORT}! 🏎️`)
+  console.log(`Listening for requests on port ${PORT}!`)
 );
