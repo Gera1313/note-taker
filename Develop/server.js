2ed8c9c9dfa005e4ebd21f0de878e336ee96eb33
Mark Anthony <touya1313@hotmail.com>
2023-08-22 15:21:39 -0600

    added port listen code

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,9 @@
 const express = require("express")
-const app = express()
 
 app.listen(3001)
+
+const app = express()
+
+app.listen(PORT, () =>
+  console.log(`Listening for requests on port ${PORT}! 🏎️`)
+);
