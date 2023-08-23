6bd710e35c903d419efe4f6273c5e821e62a3a11
Mark Anthony <touya1313@hotmail.com>
2023-08-23 00:26:32 -0600

    created app.get for index and html

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,25 @@
 const express = require("express")
 
+// const apiRoutes = require("./routes/")
+const htmlRoutes = require("./routes/index")
+
 app.listen(3001)
 
 const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use("/htmlRoutes", htmlRoutes)
+// app.use("/", apiRoutes)
 
-app.use(express.static('public'));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+});
 
 app.listen(PORT, () =>
   console.log(`Listening for requests on port ${PORT}!`)
