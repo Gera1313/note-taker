161e90bb95d273b3cdf99e0cfac4eb8e4eb48a41
Mark Anthony <touya1313@hotmail.com>
2024-01-13 23:48:08 -0700

    formatted one file

--- Develop/server.js
+++ Develop/server.js
@@ -1,31 +1,29 @@
-const express = require("express")
+const express = require("express");
 
 // const apiRoutes = require("./routes/")
-const htmlRoutes = require("./routes/index")
+const htmlRoutes = require("./routes/index");
 
-const PORT = 3001
+const PORT = 3001;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 // app.use("/htmlRoutes", htmlRoutes)
 // app.use("/", apiRoutes)
 // app.use('/api', api)
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.listen(PORT, () =>
-  console.log(`Listening for requests on port ${PORT}!`)
-);
+app.listen(PORT, () => console.log(`Listening for requests on port ${PORT}!`));
