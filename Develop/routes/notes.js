90a4e792febf080ce0aa158cb1c17b317aec0986
Mark Anthony <touya1313@hotmail.com>
2024-01-18 23:56:47 -0700

    chaning notes.js and index.js codes

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -5,12 +5,12 @@ const db = require("../db/db.json");
 
 router.get("/notes", (req, res) => {
     console.log('GET /notes route hit');
-    res.json(db)
+    res.json(db);
 });
 
 router.post("/notes", (req, res) => {
     console.log('POST /notes route hit');
-    const note = req.body; 
+    const note = req.body;
     note.id = (db.length + 1).toString();
 
     db.push(note);
@@ -21,9 +21,9 @@ router.post("/notes", (req, res) => {
             if (err) throw err;
             res.status(200).json(note);
         }
-    ); 
-}); 
+    );
+});
 
-module.exports = router; 
+module.exports = router;
 
 // NOTE: looked at INS and STU code from the Modules to get boilerplate code and examples. 
--- Develop/server.js
+++ Develop/server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.use("/", htmlRoutes);
 app.use("/api", notesRoutes);
