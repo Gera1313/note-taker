88a20937b9242ff4e5a4b2f801eea494370c1eb1
Mark Anthony <touya1313@hotmail.com>
2024-01-19 00:03:19 -0700

    modified code in server.js but still keep getting error. going to sleep

--- Develop/server.js
+++ Develop/server.js
@@ -1,31 +1,55 @@
+// const express = require("express");
+// const path = require("path");
+// const htmlRoutes = require("./routes/index");
+// const notesRoutes = require("./routes/notes");
+
+// const PORT = 3001;
+
+// const app = express();
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use("/", htmlRoutes);
+// app.use("/api", notesRoutes);
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public/index.html"));
+// });
+
+// app.get("/notes", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public/notes.html"));
+// });
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public/index.html"));
+// });
+
+// app.listen(PORT, () => console.log(`Listening for requests on port http://localhost:${PORT}`));
+
 const express = require("express");
 const path = require("path");
 const htmlRoutes = require("./routes/index");
 const notesRoutes = require("./routes/notes");
 
 const PORT = 3001;
-
 const app = express();
 
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("public"));
 
+// Use the routes
 app.use("/", htmlRoutes);
 app.use("/api", notesRoutes);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
+// Define routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, () => console.log(`Listening for requests on port http://localhost:${PORT}`));
