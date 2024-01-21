d58593ffbd8ff724e4d42d83c0eb96d970d36b53
Mark Anthony <touya1313@hotmail.com>
2024-01-21 00:57:09 -0700

    formatted the code with prettier

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,19 +1,19 @@
-const router = require('express').Router();
-const fs = require('fs');
+const router = require("express").Router();
+const fs = require("fs");
 
 router.get("/api/notes", async (req, res) => {
-    const db = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
-    res.json(db);
+  const db = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  res.json(db);
 });
 
 router.post("/api/notes", (req, res) => {
-    const db = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
-    const note = req.body;
-    note.id = (db.length + 1).toString();
+  const db = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  const note = req.body;
+  note.id = (db.length + 1).toString();
 
-    db.push(note);
-    fs.writeFileSync("db/db.json", JSON.stringify(db));
-    res.json(db);
+  db.push(note);
+  fs.writeFileSync("db/db.json", JSON.stringify(db));
+  res.json(db);
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -4,13 +4,14 @@ const notesRoutes = require("./routes/notes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
-app.use(htmlRoutes); 
+app.use(htmlRoutes);
 app.use(notesRoutes);
 
-
-app.listen(PORT, () => console.log(`Listening for requests on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Listening for requests on http://localhost:${PORT}`)
+);
