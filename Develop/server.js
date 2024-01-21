960660ca3128546b3fcccfa687557a6bf1467516
Mark Anthony <touya1313@hotmail.com>
2024-01-20 23:15:48 -0700

    going to try a new method

--- Develop/server.js
+++ Develop/server.js
@@ -1,25 +1,24 @@
 const express = require("express");
-const path = require("path");
+// const path = require("path");
 const htmlRoutes = require("./routes/index");
 const notesRoutes = require("./routes/notes");
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // Parse JSON and form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // false? 
 
 // Use routes
-app.use("/", htmlRoutes);
-app.use("/api", notesRoutes);
+app.use(htmlRoutes); //app.use("/", htmlRoutes);
+app.use(notesRoutes); //app.use("/api", notesRoutes);
 
 // Default route to serve index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public/index.html"));
+// });
 
 app.listen(PORT, () => console.log(`Listening for requests on http://localhost:${PORT}`));
