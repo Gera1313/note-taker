391079bf1d132b137e8374dbe1c88683aec0b3d6
Mark Anthony <touya1313@hotmail.com>
2024-01-21 01:34:35 -0700

    starting the delete route to delete notes

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const fs = require("fs");
 
+// reads the db.json file and retuns all saved notes as JSON
 router.get("/api/notes", async (req, res) => {
   const db = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
   res.json(db);
 });
 
+// Receives a new note to save on the request body, adds it to the db.json file, 
+// then returns the new note to the client.
 router.post("/api/notes", (req, res) => {
   const db = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
   const note = req.body;
@@ -14,6 +17,11 @@ router.post("/api/notes", (req, res) => {
   db.push(note);
   fs.writeFileSync("db/db.json", JSON.stringify(db));
   res.json(db);
+});
+
+// Delete route
+router.delete("/api/notes/:id", (req, res) => {
+    
 });
 
 module.exports = router;
