54a0f19b8f2dbae7fe26da1c8ac904541962325f
Mark Anthony <touya1313@hotmail.com>
2024-01-21 00:34:12 -0700

    modified code in notes.js

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,27 +1,21 @@
 const router = require('express').Router();
 const fs = require('fs');
-const path = require('path');
-const db = require("../db/db.json");
+// const path = require('path');
+// const db = require("../db/db.json");
 
 router.get("/api/notes", async (req, res) => {
     const db = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     res.json(db);
 });
 
-router.post("/notes", (req, res) => {
-    // console.log('POST /notes route hit');
+router.post("/api/notes", (req, res) => {
+    const db = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     const note = req.body;
     note.id = (db.length + 1).toString();
 
     db.push(note);
-    fs.writeFile(
-        path.join(__dirname, "../db/db.json"),
-        JSON.stringify(db, null, 2),
-        (err) => {
-            if (err) throw err;
-            res.status(200).json(note);
-        }
-    );
+    fs.writeFileSync("db/db.json", JSON.stringify(db));
+    res.json(db);
 });
 
 module.exports = router;
