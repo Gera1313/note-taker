26ce0fcfeb6bc9b21e34e871853daa855e4bcaa9
Mark Anthony <touya1313@hotmail.com>
2024-01-20 20:19:55 -0700

    trying a new approach

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const htmlRoutes = require("./routes/index");
 const notesRoutes = require("./routes/notes");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Serve static files
